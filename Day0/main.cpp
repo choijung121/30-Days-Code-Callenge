4002f7bb3053a4145356506081556e028463c21d
Jung Choi <56977454+choijung121@users.noreply.github.com>
2020-07-31 08:30:05 -0600

    first commit

---
+++ Day0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Day1
+//
+//  Created by Jung Choi on 7/31/20.
+//  Copyright © 2020 Jung Choi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
